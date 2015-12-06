449bfae7e58f054a4f98bc303cd69046896409d0
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-12-06 10:45:24 +0400

    Add localized title for controller.

---
+++ Pod/Classes/Controllers/UIViewController+Localization.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Localization.h
+//  Pods
+//
+//  Created by vlad gorbenko on 12/6/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Localization)
+
+@end
---
+++ Pod/Classes/Controllers/UIViewController+Localization.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Localization.m
+//  Pods
+//
+//  Created by vlad gorbenko on 12/6/15.
+//
+//
+
+#import "UIViewController+Localization.h"
+
+@implementation UIViewController (Localization)
+
+@end
