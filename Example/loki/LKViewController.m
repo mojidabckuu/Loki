2841925dca010f257fcfe44af652eec449a18708
alex <alex@farabi.ae>
2016-06-23 11:58:36 +0400

    Example

--- Example/loki/LKViewController.m
+++ Example/loki/LKViewController.m
@@ -8,7 +8,13 @@
 
 #import "LKViewController.h"
 
+@import Loki;
+
 @interface LKViewController ()
+
+@property (strong, nonatomic) LKLanguage * currentLanguage;
+@property (strong, nonatomic) LKLanguage * english;
+@property (strong, nonatomic) LKLanguage * arabic;
 
 @end
 
@@ -17,13 +23,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.english = [[LKLanguage alloc] initWithName:@"English" code:@"en" title:@"English"];
+    self.arabic = [[LKLanguage alloc] initWithName:@"Ar" code:@"Arabic" title:@"Arabic"];
+    
+    [LKManager addLanguage:self.english];
+    [LKManager addLanguage:self.arabic];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(languageDidChange:) name:LKLanguageDidChangeNotification object:nil];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Setters
+
+- (void)setCurrentLanguage:(LKLanguage *)currentLanguage {
+    _currentLanguage = currentLanguage;
+    [LKManager sharedInstance].currentLanguage = _currentLanguage;
+}
+
+#pragma mark - Utils
+
+- (void)languageDidChange:(NSNotification*)notification {
+    [self.view localizeSubviews];
+    [self.navigationController.navigationBar localizeSubviews];
+}
+
+- (IBAction)changeLanguage:(id)sender {
+    self.currentLanguage = self.currentLanguage == self.english ? self.arabic : self.english;
 }
 
 @end
